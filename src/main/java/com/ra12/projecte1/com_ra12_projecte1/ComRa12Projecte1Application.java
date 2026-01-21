18f5ef8a570c685538de2471c1ccbd150126170f
ldloler <marc.be.pi@gmail.com>
2026-01-21 19:12:31 +0100

    primer commit

---
+++ src/main/java/com/ra12/projecte1/com_ra12_projecte1/ComRa12Projecte1Application.java
@@ -0,0 +1,13 @@
+package com.ra12.projecte1.com_ra12_projecte1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComRa12Projecte1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComRa12Projecte1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ra12/projecte1/com_ra12_projecte1/ComRa12Projecte1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra12.projecte1.com_ra12_projecte1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComRa12Projecte1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
