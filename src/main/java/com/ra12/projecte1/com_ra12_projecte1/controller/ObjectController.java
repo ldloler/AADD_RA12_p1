145390b59c97f87158706915d806a022fae08c5d
ldloler <marc.be.pi@gmail.com>
2026-01-28 19:20:26 +0100

    Created endpoints i funcionalitats Integrants 1

--- src/main/java/com/ra12/projecte1/com_ra12_projecte1/controller/ObjectController.java
+++ src/main/java/com/ra12/projecte1/com_ra12_projecte1/controller/ObjectController.java
@@ -1,5 +1,7 @@
 package com.ra12.projecte1.com_ra12_projecte1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ra12.projecte1.com_ra12_projecte1.dto.ObjectRequest;
 import com.ra12.projecte1.com_ra12_projecte1.model.Objecte;
 import com.ra12.projecte1.com_ra12_projecte1.service.ObjectService;
 
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
@@ -23,6 +28,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ObjectController {
     @Autowired
     ObjectService objectService;
+
+    @GetMapping("/objecte")
+    public ResponseEntity<List<Objecte>> getAllObjecte() {
+        List<Objecte> response = objectService.getAllObjectes();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+    
+    @GetMapping("/objecte/{id}")
+    public ResponseEntity<Objecte> getAllObjecte(@PathVariable Long id) {
+        Objecte response = objectService.getObjecteByID(id);
+        if (response == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/objecte")
+    public ResponseEntity<String> createObject(@RequestBody ObjectRequest objecte) {
+        int created = objectService.createObjecte(objecte);
+        if (created == 0) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No s'ha pogut crear l'objecte");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body("S'ha creat l'objecte " + objecte.getTitulo() + " correctament.");
+    }
+
+    @PostMapping("/objecte/batch")
+    public ResponseEntity<String> createObjectsBatch(@RequestParam MultipartFile csvFile) {
+        int created = objectService.createObjectesBatch(csvFile);
+        if (created == 0) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("No s'han pogut crear els objectes");
+        }
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body("S'han creat " + created + " objectes");
+    }
+    
 
     @PostMapping("/objecte/{id}/image")
     public ResponseEntity<String> postImageObject(@PathVariable Long id, @RequestParam MultipartFile image) {
--- src/main/java/com/ra12/projecte1/com_ra12_projecte1/service/ObjectService.java
+++ src/main/java/com/ra12/projecte1/com_ra12_projecte1/service/ObjectService.java
@@ -1,5 +1,7 @@
 package com.ra12.projecte1.com_ra12_projecte1.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ra12.projecte1.com_ra12_projecte1.dto.ObjectRequest;
 import com.ra12.projecte1.com_ra12_projecte1.logs.ObjecteLogs;
 import com.ra12.projecte1.com_ra12_projecte1.model.Objecte;
 import com.ra12.projecte1.com_ra12_projecte1.repository.ObjecteRepository;
@@ -22,9 +25,72 @@ public class ObjectService {
     @Autowired
     ObjecteLogs objecteLogs;
 
+    public List<Objecte> getAllObjectes() {
+        objecteLogs.info("S'han consultat tots els Objectes", "ObjectService", "getAllObjectes");
+        return objecteRepository.findAll();
+    }
 
-    public int updatingObjecte(Long id, Objecte objecte){ 
-        if(objecteRepository.findUserById(id) == null){
+    public Objecte getObjecteByID(Long id) {
+        if (objecteRepository.findObjecteById(id) == null) {
+            objecteLogs.error("No s'ha pogut trobar l'objecte", "ObjectService", "getObjectByID");
+            return null;
+        }
+        objecteLogs.info("S'ha consultat l'objecte correctament.", "ObjectService", "getObjectByID");
+        return objecteRepository.findObjecteById(id);
+    }
+
+    public int createObjecte(ObjectRequest objecte) {
+        int isSaved = objecteRepository.save(objecte);
+        if (isSaved == 0) {
+            objecteLogs.error("No s'ha guardat l'objecte" + objecte.getTitulo(), "ObjectService", "createObjecte");
+        } else {
+            objecteLogs.info("S'ha guardat l'objecte " + objecte.getTitulo() + " correctament.", "ObjectService",
+                    "createObjecte");
+        }
+        return isSaved;
+    }
+
+    public int createObjectesBatch(MultipartFile csvFile) {
+        objecteLogs.info("Inserint Objectes en bloc", "ObjectService", "createObjectesBatch");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(csvFile.getInputStream()))) {
+            String linia = br.readLine();
+            int numObjectesCreats = 0;
+
+            while ((linia = br.readLine()) != null) {
+
+                // Separar per comes (CSV simple)
+                String[] camps = linia.split(",");
+
+                ObjectRequest new_objecte = new ObjectRequest(camps[0], camps[1], camps[2], camps[3], camps[4],
+                        Boolean.parseBoolean(camps[5]));
+                if (createObjecte(new_objecte) == 0) {
+                    continue;
+                }
+                numObjectesCreats++;
+
+            }
+
+            objecteLogs.info("S'han guardat " + numObjectesCreats + " objectes", "ObjectService",
+                    "createObjectesBatch");
+
+            Path pathDirectori = Paths.get("com_ra12_projecte1/private/csv_processed");
+            Path pathFitxer = Paths.get("com_ra12_projecte1/private/csv_processed/"
+                    + System.currentTimeMillis() + csvFile.getOriginalFilename());
+
+            Files.createDirectories(pathDirectori);
+            Files.copy(csvFile.getInputStream(), pathFitxer);
+
+            objecteLogs.info("S'ha guardat el csv a " + pathFitxer.toString(), "ObjectService",
+                    "createObjectesBatch");
+
+            return numObjectesCreats;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public int updatingObjecte(Long id, Objecte objecte) {
+        if (objecteRepository.findObjecteById(id) == null) {
             objecteLogs.error("No s'ha pogut actualitzar l'usuari", "ObjectService", "updatingObjecte");
             return 0;
         }
@@ -32,8 +98,8 @@ public class ObjectService {
         return objecteRepository.update(id, objecte);
     }
 
-    public int deletingById(Long id){
-        if (objecteRepository.findUserById(id) == null) {
+    public int deletingById(Long id) {
+        if (objecteRepository.findObjecteById(id) == null) {
             objecteLogs.error("No s'ha pogut esborrar l'objecte.", "ObjectService", "deletingById");
             return 0;
         }
@@ -41,7 +107,7 @@ public class ObjectService {
         return objecteRepository.delete(id);
     }
 
-    public int deletingAll(){
+    public int deletingAll() {
         List<Objecte> tots = objecteRepository.findAll();
 
         if (tots == null || tots.isEmpty()) {
@@ -52,11 +118,11 @@ public class ObjectService {
         return objecteRepository.delete();
     }
 
-    public String uploadingImage(Long id, MultipartFile image){
-        Objecte objecte = objecteRepository.findUserById(id);
+    public String uploadingImage(Long id, MultipartFile image) {
+        Objecte objecte = objecteRepository.findObjecteById(id);
         int numReg = 0;
 
-        if(objecte == null){
+        if (objecte == null) {
             objecteLogs.error("Error al carregar la imatge. No s'ha afegit cap.", "ObjecteService", "uploadingImage");
             return "";
         }
@@ -75,12 +141,12 @@ public class ObjectService {
         return pathFinal;
     }
 
-    public String savingImage(MultipartFile image){
+    public String savingImage(MultipartFile image) {
 
         String path = "/private/imageObjecte";
 
         try {
-            if(image == null || image.isEmpty()){
+            if (image == null || image.isEmpty()) {
                 objecteLogs.error("Error al carregar la imatge. No s'ha afegit cap.", "ObjecteService", "savingImage");
                 return null;
             }
@@ -89,7 +155,7 @@ public class ObjectService {
 
             Path fileDir = Paths.get(path);
 
-            if(!Files.exists(fileDir)){
+            if (!Files.exists(fileDir)) {
                 Files.createDirectories(fileDir);
             }
 
@@ -99,8 +165,9 @@ public class ObjectService {
             String newFile = "image_" + baseNom + System.currentTimeMillis() + extensio;
 
             Path filePath = fileDir.resolve(newFile);
-            Files.copy(image.getInputStream(), filePath,StandardCopyOption.REPLACE_EXISTING);
-            objecteLogs.info("S'ha guardat correctament la imatge en el path: " + filePath, "ObjecteService", "savingImage");
+            Files.copy(image.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            objecteLogs.info("S'ha guardat correctament la imatge en el path: " + filePath, "ObjecteService",
+                    "savingImage");
             return path;
 
         } catch (Exception e) {
